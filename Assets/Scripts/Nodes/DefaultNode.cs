b822af4612618c229af0c74f567803bef0884d37
Ali Unal <0033484@gmail.com>
2022-10-08 15:10:23 +0300

    Node click animation added

--- Assets/Scripts/Nodes/DefaultNode.cs
+++ Assets/Scripts/Nodes/DefaultNode.cs
@@ -1,12 +1,36 @@
 using UnityEngine;
+using DG.Tweening;
 
 namespace GridBlast.GridSystem.Nodes
 {
     public class DefaultNode : Node, IClickable
     {
+        [SerializeField] private SpriteRenderer icon;
+
+        [SerializeField] private float clickAnimationScaleAmount;
+        [SerializeField] private float clickAnimationDuration;
+
+        public bool Clicked { get; set; }
+
+        private void Start()
+        {
+            icon.enabled = false;
+        }
+
         public void Click()
         {
-            Debug.Log("clicked");
+            if(Clicked)
+                return;
+
+            icon.enabled = true;
+
+            Vector3 targetIconScale = icon.transform.localScale;
+            icon.transform.localScale = Vector3.zero;
+
+            transform.DOPunchScale(Vector3.one * clickAnimationScaleAmount, clickAnimationDuration, 1, 1);
+            icon.transform.DOScale(targetIconScale, clickAnimationDuration).SetEase(Ease.OutBack);
+
+            Clicked = true;
         }
     }
 }
--- Assets/Scripts/Nodes/IClickable.cs
+++ Assets/Scripts/Nodes/IClickable.cs
@@ -2,6 +2,8 @@ namespace GridBlast.GridSystem.Nodes
 {
     public interface IClickable
     {
+        public bool Clicked { get; set; }
+
         public void Click();
     }
 }
