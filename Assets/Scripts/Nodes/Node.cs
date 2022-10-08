05c1e6cfc71a02f50a09530c45b695b90ca99bf1
Ali Unal <0033484@gmail.com>
2022-10-08 15:59:44 +0300

    Clicked node evaluation added

--- Assets/Scripts/Nodes/Node.cs
+++ Assets/Scripts/Nodes/Node.cs
@@ -12,5 +12,33 @@ namespace GridBlast.GridSystem.Nodes
         {
             neighbours.Add(node);
         }
+
+        /// <summary>
+        /// Returns all clicked neighbours of this node excluding this node itself
+        /// </summary>
+        /// <returns></returns>
+        public List<Node> GetClickedNeighbours()
+        {
+            List<Node> clickedNeighbours = new List<Node>();
+            List<Node> nodesToEvaluate = new List<Node>();
+
+            foreach(var clickedNeighbour in neighbours.FindAll(x => !clickedNeighbours.Contains(x) && x.TryGetComponent(out IClickable clickable) && clickable.Clicked))
+            {
+                nodesToEvaluate.Add(clickedNeighbour);
+            }
+
+            while(nodesToEvaluate.Count > 0)
+            {
+                foreach(var clickedNeighbour in nodesToEvaluate[0].Neighbours.FindAll(x => x != this && !clickedNeighbours.Contains(x) && x.TryGetComponent(out IClickable clickable) && clickable.Clicked))
+                {
+                    nodesToEvaluate.Add(clickedNeighbour);
+                }
+
+                clickedNeighbours.Add(nodesToEvaluate[0]);
+                nodesToEvaluate.RemoveAt(0);
+            }
+
+            return clickedNeighbours;
+        }
     }
 }
